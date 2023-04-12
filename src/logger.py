2ea64f311b2f9d741870c64406aca05378704df0
Upendra Singh <u.gautam2006@gmail.com>
2023-04-12 22:47:27 +0530

    commited all file

--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs", LOG_FILE)
-os.makedirs(logs_path, exit_ok=True)
+os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH=os.path.join(logs_path, LOG_FILE)
 
